ae898c08aaa1c5c8fe1d7cca9f999402eea4f4fe
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2023-10-13 16:16:30 -0400

    Update simplephotongen.cc
    fixed all my histograms. previously the 200 in my exponential was in GeV. it is supposed to be 200 MeV and was fixed (0.2GeV).  for now I will take this and attempt to run my pioncode using the various weighting methods. lets see

--- simplephotongen.cc
+++ simplephotongen.cc
@@ -11,8 +11,10 @@
 #include "TStopwatch.h"
 #include "TGraph.h"
 #include "TCanvas.h"
+#include "Math/Vector4D.h"
+#include "TLorentzVector.h"
 
-int main(){ // Begin main program.
+void simplephotongen(){ // Begin main program.
 	// auto start = std::chrono::steady_clock::now();
 	// std::cout << "Start Time (sec) = " << start.count() << std::endl;
 	// int prefactor =3 ;
@@ -28,7 +30,7 @@ int main(){ // Begin main program.
 	//-------------------- power law param
 	float plaw_param = 7.1;
 	//-------------------- exp param
-	float exp_param = 7.1;
+	float exp_param = 200;
 	//--------------------Alternative paramaterization, woods saxon+hagedorn+power law
 	double t = 4.5;
 	double w = 0.114;
@@ -43,13 +45,22 @@ int main(){ // Begin main program.
 	float smear_factor_c = 0.028; // first parameter in test beam parameterization?
 	float smear_factor_b = 0.155;
 
-	TFile *output = new TFile(pioncode / rootfiles / simplephotoncheck.root, "recreate");
-	TTree *tree = new TTree("tree", "tree");
+	//TFile *output = new TFile(pioncode / rootfiles / simplephotoncheck.root, "recreate");
+	//TTree *tree = new TTree("tree", "tree");
 
 	TH1 *h1 = new TH1D("h1", "gamma Pt, flat", n_bins, PT_Min, PT_Max);
-	TH1 *h2 = new TH1D("h1", "gamma Pt, power weight", n_bins, PT_Min, PT_Max);
-	TH1 *h3 = new TH1D("h2", "gamma Pt, exp weight", n_bins, PT_Min, PT_Max);
-	TH1 *h4 = new TH1D("h3", "gamma Pt, w.s.h.p. weight", n_bins, PT_Min, PT_Max);
+	//weighted
+	TH1 *h2 = new TH1D("h2", "gamma Pt, exp weight", n_bins, PT_Min, PT_Max);
+	TH1 *h3 = new TH1D("h3", "gamma Pt, power weight", n_bins, PT_Min, PT_Max);
+	TH1 *h4 = new TH1D("h4", "gamma Pt, w.s.h.p. weight", n_bins, PT_Min, PT_Max);
+	//weighted+smeared
+	TH1 *h5 = new TH1D("h5", "gamma Pt smeared, exp weight", n_bins, PT_Min, PT_Max);
+	TH1 *h6 = new TH1D("h6", "gamma Pt smeared, power weight", n_bins, PT_Min, PT_Max);
+	TH1 *h7 = new TH1D("h7", "gamma Pt smeared, w.s.h.p. weight", n_bins, PT_Min, PT_Max);
+	//weighted+smeared/weighted ratio
+	TH1 *h8 = new TH1D("h8", "gamma Pt smeared+weighted/weighted, exp weight", n_bins, PT_Min, PT_Max);
+	TH1 *h9 = new TH1D("h9", "gamma Pt smeared+weighted/weighted, power weight", n_bins, PT_Min, PT_Max);
+	TH1 *h10 = new TH1D("h10", "gamma Pt smeared+weighted/weighted, w.s.h.p. weight", n_bins, PT_Min, PT_Max);
 
 	std::random_device rd;		// generate a random number to seed random generation
 	std::random_device rdgamma; // generate a random number to seed random generation of daughter gamma for smearing
@@ -59,54 +70,102 @@ int main(){ // Begin main program.
 	std::normal_distribution<double> gammadis(0.0, 1.0); // generate normal distribution for gamma smearing, mean zero, variance 1
 	std::uniform_real_distribution<> pdis(PT_ratio, 1.0);
 	std::uniform_real_distribution<> adis(0.0, 2 * M_PI);
-
+	float pz =0.0;
 	for (int i = 0; i < NPhotons; i++){
-		double azimuthal_ang = adis(gen); // generate a random angle from 0 to 2pi
-		double Pt = PT_Max * pdis(gen);
-		h1->Fill(Pt, weight_function1);
-		h1->Fill(Pt, weight_function1);
-
-		double px = Pt * cos(azimuthal_ang); // remember p=hbar k for photons
-		double py = Pt * sin(azimuthal_ang);
-		// double E=Pt;// + pi0_pz*pi0_pz// for now just use Pt instead of E
-		float gamma1_4mom[4] = [ Pt, px, py, 0 ];
-		float gamma2_4mom[4] = [ Pt, -px, -py, 0 ];
-
+		float azimuthal_ang = adis(gen); // generate a random angle from 0 to 2pi
+		float Pt = PT_Max * pdis(gen);
+		h1->Fill(Pt);
+		h1->Fill(Pt);
 		//------------------------------smearing photons
-		scale_factor1 = sqrt(pow(smear_factor_b, 2) / gamma1_4mom[1] + pow(smear_factor_c, 2) + pow(smear_factor_d, 2));
-		scale_factor2 = sqrt(pow(smear_factor_b, 2) / gamma2_4mom[1] + pow(smear_factor_c, 2) + pow(smear_factor_d, 2));
-		smear_factor1 = scale_factor1 * gammadis(gen_gamma) + 1;
-		smear_factor2 = scale_factor2 * gammadis(gen_gamma) + 1;
-		float gamma1_4mom_smeared[4] = smear_factor1 * gamma1_4mom;
-		float gamma2_4mom_smeared[4] = smear_factor1 * gamma1_4mom;
+		float scale_factor1 = sqrt(pow(smear_factor_b, 2) / Pt + pow(smear_factor_c, 2) + pow(smear_factor_d, 2));
+		//float scale_factor2 = sqrt(pow(smear_factor_b, 2) / Pt + pow(smear_factor_c, 2) + pow(smear_factor_d, 2));// these are naturally the same if I say pz=0
+		float smear_factor1 = scale_factor1 * gammadis(gen_gamma) + 1;
+		float smear_factor2 = scale_factor1 * gammadis(gen_gamma) + 1;
+		float smeared_Pt1=Pt*smear_factor1;
+		float smeared_Pt2=Pt*smear_factor2;
+
 		//------------------------------find energies/ Pt
-		float Pt1 = sqrt(pow(gamma1_4mom_smeared[1], 2) + pow(gamma1_4mom_smeared[2], 2)); // will be the same. maybe entirely redundant to do this.
-		float Pt2 = sqrt(pow(gamma2_4mom_smeared[1], 2) + pow(gamma1_4mom_smeared[2], 2));
-		double weight_function1 = exp(-Pt / 7.1);
-		double weight_function2 = 1 / (pow(Pt, -7.1));
+		double weight_function1 = exp(-Pt/0.200 );// exponential. scaled it to 200
+		//note hagedorn should be dominant up to ~5 GeV from wf3. we are using an exponential instead
+		double weight_function2 = 1 / (pow(Pt, 7.1)); //power law
+		// power law is becomes important around 5 GeV
 		double weight_function3 = ((1 / (1 + exp((Pt - t) / w))) * A / pow(1 + Pt / p0, m_param) + (1 - (1 / (1 + exp((Pt - t) / w)))) * B / (pow(Pt, n)));
-		h2->Fill(Pt1, weight_function1);
-		h2->Fill(Pt2, weight_function1);
+		h2->Fill(Pt, weight_function1);
+		h2->Fill(Pt, weight_function1);
+		h5->Fill(smeared_Pt1, weight_function1);
+		h5->Fill(smeared_Pt2, weight_function1);
 
-		h3->Fill(Pt1, weight_function2);
-		h3->Fill(Pt2, weight_function2);
+		h3->Fill(Pt, weight_function2);
+		h3->Fill(Pt, weight_function2);
+		h6->Fill(smeared_Pt1, weight_function2);
+		h6->Fill(smeared_Pt2, weight_function2);
 
-		h4->Fill(Pt1, Pt * weight_function3);
-		h4->Fill(Pt2, Pt * weight_function3);
+		h4->Fill(Pt, Pt*weight_function3);
+		h4->Fill(Pt, Pt*weight_function3);
+		h7->Fill(smeared_Pt1, Pt * weight_function3);
+		h7->Fill(smeared_Pt2, Pt * weight_function3);
 	}
 
 
-	TCanvas* c1 = new TCanvas("c1", "c1", 800, 800);
+	TCanvas* c1 = new TCanvas("c1", "c1", 800, 800);//unsmeared pTs
 	// c1->SetFillColor(42);
 	c1->Divide(2, 2); // nx, ny
 	c1->cd(1);
 	h1->Draw("colz");
 	c1->cd(2);
+	gPad->SetLogy();
 	h2->Draw("colz");
 	c1->cd(3);
+	gPad->SetLogy();
 	h3->Draw("colz");
 	c1->cd(4);
+	gPad->SetLogy();
 	h4->Draw("colz");
+
+	TCanvas* c3 = new TCanvas("c3", "c3", 900, 300);//smeared Pts
+	c3->Divide(3, 1); // nx, ny
+	c3->cd(1);
+	gPad->SetLogy();
+	h5->Draw("colz");
+	c3->cd(2);
+	gPad->SetLogy();
+	h6->Draw("colz");
+	c3->cd(3);
+	gPad->SetLogy();
+	h7->Draw("colz");
+
+	TCanvas* c2 = new TCanvas("c2", "c2", 900, 300);//ratios
+	// c1->SetFillColor(42);
+	c2->Divide(3, 1); // nx, ny
+	c2->cd(1);
+	//h8->Clone("h5");
+	//TH1D* h8 = (TH1D*)h5->Clone("h8");
+	//h5->Scale(pow(10, 14));
+	//h2->Scale(pow(10, 14));
+	//h2->SetBinContent(0, 344);
+	//h2->SetBinContent(65,2.51738E-122);
+	//h8->Divide(h2);
+	double h8binconttemp;
+	for(int j=1;j<n_bins+1;j++){
+		h8binconttemp=h5->GetBinContent(j)/h2->GetBinContent(j);
+		h8->SetBinContent(j,h8binconttemp);
+		std::cout << h5->GetBinContent(j) << " Divided by " << h2->GetBinContent(j) << " gives " << h8->GetBinContent(j) << std::endl;
+	}
+	//h8->Scale(pow(10, 14));
+	gPad->SetLogy();
+	h8->Draw("colz");
+	//h8->DrawNormalized("colz");
+	c2->cd(2);
+	//h9->Clone("h6");
+	h9->Divide(h6,h3);
+	h9->Draw("colz");
+	c2->cd(3);
+	//h10->Clone("h7");
+	h10->Divide(h7,h4);
+	h10->Draw("colz");
+
+	
+
 	double_t realtime = timer.RealTime();
 	double_t cputime = timer.CpuTime();
 
