364e87076268b9a4d47fd69491906a5ea3c0938b
Nikhil Kumar <nkumar7252@gmail.com>
2023-11-11 12:06:48 -0500

    Update CombinedFits.C
    new version

--- CombinedFits.C
+++ CombinedFits.C
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <TFile.h>
-#include <TH1F.h>
+#include <TH2F.h>
 #include <TCanvas.h>
 #include <TStyle.h>
 #include <TLegend.h>
+#include <TFitResult.h>
+#include <TFitResultPtr.h>
+#include <TF1.h>
 
-void PlotMultipleVersions(const std::vector<std::string>& fileNames) {
+void OverlayMeans(const std::vector<std::string>& fileNames) {
     // Create a TCanvas
-    TCanvas* canvas = new TCanvas("canvas", "Multiple Versions Plot", 800, 600);
+    TCanvas* canvas = new TCanvas("canvas", "Overlay Means", 800, 600);
     canvas->SetGrid();
     gStyle->SetOptStat(0);
 
@@ -17,41 +20,60 @@ void PlotMultipleVersions(const std::vector<std::string>& fileNames) {
     // Loop over each file
     for (size_t i = 0; i < fileNames.size(); ++i) {
         // Open the root file
-        TFile* file = new TFile(fileNames[i].c_str(), "READ");
+        TFile* pionfile = new TFile(fileNames[i].c_str(), "READ");
 
         // Check if the file is open
-        if (!file || file->IsZombie()) {
+        if (!pionfile || pionfile->IsZombie()) {
             std::cerr << "Error: Unable to open file " << fileNames[i] << std::endl;
             continue;
         }
 
-        // Retrieve the histogram
-        TH1F* hist = nullptr;
-        file->GetObject("myHist", hist);
+        // Retrieve the 2D histogram
+        TH2F *h18 =(TH2F *)pionfile->Get("h18");
+        //TH2F* hist2D = nullptr;
+        //file->GetObject("h18", hist2D);
 
         // Check if the histogram exists
-        if (!hist) {
-            std::cerr << "Error: Histogram 'myHist' not found in file " << fileNames[i] << std::endl;
+        if (!hist2D) {
+            std::cerr << "Error: Histogram 'myHist2D' not found in file " << fileNames[i] << std::endl;
             file->Close();
             continue;
         }
 
-        // Set different marker styles for each version
-        int markerStyle = 20 + i; // Marker style: 20, 21, 22, ...
-        hist->SetMarkerStyle(markerStyle);
+        // Set different line colors for each version
+        int lineColor = i + 1; // Line color: 1, 2, 3, ...
+        hist2D->SetLineColor(lineColor);
 
-        // Plot the histogram
-        if (i == 0) {
-            hist->Draw("P"); // Draw with markers for the first version
-        } else {
-            hist->Draw("P SAME"); // Draw with markers and on the same canvas for subsequent versions
+        // Loop over each bin in the X direction
+        for (int binX = 1; binX <= hist2D->GetNbinsX(); ++binX) {
+            // Project along Y for each binX
+            TH1D* yProjection = hist2D->ProjectionY(Form("YProjection_%d_%d", i, binX), binX, binX, "");
+
+            // Fit the Y projection with a Gaussian
+            yProjection->Fit("gaus", "Q");
+
+            // Access the fit parameters
+            TF1* fitFunc = yProjection->GetFunction("gaus");
+
+            // Check if the fit function is valid
+            if (fitFunc) {
+                // Overlay the mean on the same canvas
+                if (i == 0 && binX == 1) {
+                    yProjection->Draw("HIST"); // Draw histogram for the first version and binX
+                } else {
+                    yProjection->Draw("HIST SAME"); // Draw subsequent histograms on the same canvas
+                }
+
+                // Add an entry to the legend
+                legend->AddEntry(yProjection, Form("Version %zu, BinX %d", i, binX), "L");
+            }
+
+            // Clean up Y projection
+            delete yProjection;
         }
 
-        // Add an entry to the legend
-        legend->AddEntry(hist, Form("Version %zu", i), "P");
-
         // Close the file
-        file->Close();
+        pionfile->Close();
     }
 
     // Draw the legend
@@ -60,20 +82,19 @@ void PlotMultipleVersions(const std::vector<std::string>& fileNames) {
     // Show the canvas
     canvas->Update();
     canvas->Modified();
-    canvas->Print("MultipleVersionsPlot.pdf");
-    //canvas->SaveAs(Form("pioncode/canvas_pdf/Alt_Projection_%s.pdf", pdfname));
-		
+    canvas->Print("OverlayMeansPlot.pdf");
+
     // Clean up
     delete canvas;
     delete legend;
 }
 
-int main() {
+int CombinedFits() {
     // List of root file names
     std::vector<std::string> fileNames = {"pioncode/Pi0FastMC_0.155000EXP.root", "pioncode/Pi0FastMC_0.155000POWER.root", "pioncode/Pi0FastMC_0.155000WSHP.root"};
 
-    // Plot the histograms from different versions
-    PlotMultipleVersions(fileNames);
+    // Overlay the means for each bin
+    OverlayMeans(fileNames);
 
     return 0;
 }
