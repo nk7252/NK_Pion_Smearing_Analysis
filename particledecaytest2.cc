6be539505d6038b0c67cfbe2dd164b33cdd9c730
Nikhil Kumar <nkumar7252@gmail.com>
2024-01-19 13:03:02 -0500

    Update particledecaytest2.cc

--- particledecaytest2.cc
+++ particledecaytest2.cc
@@ -73,7 +73,7 @@ int main(){
 	float smear_factor_a = 0;
 	float smear_factor_d = 0.02;  // 0.02;// test trying to include the beam momentum resolution.. will set to zero for now
 	float smear_factor_c = 0.028; // first parameter in test beam parameterization?
-	float posit_smearingFactor = 0.1; // Example smearing factor for position
+	float posit_smearingFactor = 1.0; // Example smearing factor for position
 
 	//std::cout << "Processing: " << WeightNames[weightmethod] << std::endl;
 	//----------------------pion spectrum function for clusteroverlay
