e260082bfb46c59f747db6a59f9b7adf01ffd176
Nikhil Kumar <nkumar7252@gmail.com>
2024-03-09 19:48:26 -0500

    Update TH3Fitter.cc

--- TH3Fitter.cc
+++ TH3Fitter.cc
@@ -485,7 +485,6 @@ std::vector<TCanvas*> DrawBestHistogram(TH1D* hProjZ, double minMass, double max
 
     // Draw everything and add canvases to vector of canvases
     std::vector<TCanvas*> canvases;
-    canvases=nullptr;
     //-------------------------------------------------------------------------------------------canvas 1
     TCanvas *c1 = new TCanvas("c1", "Fits", 800, 600);
     hProjZ->Draw("E");
