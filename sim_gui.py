5287f0d9b3840960b10304ef64d9974c336b6ddf
Nikhil Kumar <nkumar7252@gmail.com>
2024-06-22 23:58:45 -0400

    format fix and added units in places

--- sim_gui.py
+++ sim_gui.py
@@ -14,7 +14,7 @@ from PyQt5.QtWidgets import (
     QDoubleSpinBox,
     QSpinBox,
     QFileDialog,
-    QScrollArea
+    QScrollArea,
 )
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
@@ -22,6 +22,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import matplotlib.pyplot as plt
 from datetime import datetime
+
 
 class SimulationGUI(QWidget):
 
@@ -36,7 +37,7 @@ class SimulationGUI(QWidget):
 
     def initUI(self):
         self.main_layout = QVBoxLayout(self)
-        
+
         scroll_area = QScrollArea(self)
         scroll_area.setWidgetResizable(True)
         self.main_layout.addWidget(scroll_area)
@@ -47,107 +48,179 @@ class SimulationGUI(QWidget):
         scroll_area.setWidget(content_widget)
 
         self.particleTypeInput = self.addFieldWithExplanation(
-            "Particle Type:", QComboBox(self), explanation="Select the type of particle to simulate."
+            "Particle Type:",
+            QComboBox(self),
+            explanation="Select the type of particle to simulate.",
         )
         self.particleTypeInput.addItems(["Pion", "Eta"])
 
         self.nParticlesInput = self.addFieldWithExplanation(
-            "Number of Particles:", QSpinBox(self), max_value=100000000, default_value=8000000,
-            explanation="Enter the number of particles to simulate."
+            "Number of Particles:",
+            QSpinBox(self),
+            max_value=100000000,
+            default_value=8000000,
+            explanation="Enter the number of particles to simulate.",
         )
 
         self.ptMaxInput = self.addFieldWithExplanation(
-            "PT Max (GeV):", QDoubleSpinBox(self), max_value=100, decimals=2, default_value=50,
-            explanation="Enter the maximum transverse momentum (PT) in GeV."
+            "PT Max (GeV):",
+            QDoubleSpinBox(self),
+            max_value=100,
+            decimals=2,
+            default_value=50,
+            explanation="Enter the maximum transverse momentum (PT) in GeV.",
         )
 
         self.ptMinInput = self.addFieldWithExplanation(
-            "PT Min (GeV):", QDoubleSpinBox(self), max_value=100, decimals=2, default_value=0,
-            explanation="Enter the minimum transverse momentum (PT) in GeV."
+            "PT Min (GeV):",
+            QDoubleSpinBox(self),
+            max_value=100,
+            decimals=2,
+            default_value=0,
+            explanation="Enter the minimum transverse momentum (PT) in GeV.",
         )
 
         self.weightMethodInput = self.addFieldWithExplanation(
-            "Weight Method:", QComboBox(self), explanation="Select the weighting method."
+            "Weight Method:",
+            QComboBox(self),
+            explanation="Select the weighting method.",
         )
         self.weightMethodInput.addItems(["EXP", "POWER", "WSHP", "HAGEDORN"])
 
         self.asymmCutInput = self.addFieldWithExplanation(
-            "Apply Asymm Cut:", QCheckBox(self), default_value=True,
-            explanation="Check to apply asymmetric cut."
+            "Apply Asymm Cut:",
+            QCheckBox(self),
+            default_value=True,
+            explanation="Check to apply asymmetric cut.",
         )
 
         self.asymmCutValueInput = self.addFieldWithExplanation(
-            "Asymm Cut Value:", QDoubleSpinBox(self), max_value=1, decimals=3, default_value=0.6,
-            explanation="Enter the value for the asymmetric cut."
+            "Asymm Cut Value:",
+            QDoubleSpinBox(self),
+            max_value=1,
+            decimals=3,
+            default_value=0.6,
+            explanation="Enter the value for the asymmetric cut.",
         )
 
         self.clusterOverlapInput = self.addFieldWithExplanation(
-            "Cluster Overlap:", QCheckBox(self), default_value=True,
-            explanation="Check to enable cluster overlap."
+            "Cluster Overlap:",
+            QCheckBox(self),
+            default_value=True,
+            explanation="Check to enable cluster overlap.",
         )
 
         self.clusterOverlapProbInput = self.addFieldWithExplanation(
-            "Cluster Overlap Probability:", QDoubleSpinBox(self), max_value=1, decimals=3, step=0.001, default_value=0.99,
-            explanation="Enter the probability of NO cluster overlap. i.e. 0.99 means 1% overlap probability."
+            "Cluster Overlap Probability:",
+            QDoubleSpinBox(self),
+            max_value=1,
+            decimals=3,
+            step=0.001,
+            default_value=0.99,
+            explanation="Enter the probability of NO cluster overlap. i.e. 0.99 means 1% overlap probability.",
         )
 
         self.deltaRcutMaxInput = self.addFieldWithExplanation(
-            "Delta R Cut Max:", QDoubleSpinBox(self), max_value=10, decimals=2, default_value=1.1,
-            explanation="Enter the maximum delta R cut value."
+            "Delta R Cut Max:",
+            QDoubleSpinBox(self),
+            max_value=10,
+            decimals=2,
+            default_value=1.1,
+            explanation="Enter the maximum delta R cut value.",
         )
 
         self.pt1cutInput = self.addFieldWithExplanation(
-            "PT1 Cut (GeV):", QDoubleSpinBox(self), max_value=100, decimals=2, default_value=1.5,
-            explanation="Enter the cut value for PT1 in GeV."
+            "PT1 Cut (GeV):",
+            QDoubleSpinBox(self),
+            max_value=100,
+            decimals=2,
+            default_value=1.5,
+            explanation="Enter the cut value for PT1 in GeV.",
         )
 
         self.pt2cutInput = self.addFieldWithExplanation(
-            "PT2 Cut (GeV):", QDoubleSpinBox(self), max_value=100, decimals=2, default_value=1.5,
-            explanation="Enter the cut value for PT2 in GeV."
+            "PT2 Cut (GeV):",
+            QDoubleSpinBox(self),
+            max_value=100,
+            decimals=2,
+            default_value=1.5,
+            explanation="Enter the cut value for PT2 in GeV.",
         )
 
         self.combPtCutInput = self.addFieldWithExplanation(
-            "Combined PT Cut (GeV):", QDoubleSpinBox(self), max_value=100, decimals=2, default_value=0,
-            explanation="Enter the combined PT cut value in GeV."
+            "Combined PT Cut (GeV):",
+            QDoubleSpinBox(self),
+            max_value=100,
+            decimals=2,
+            default_value=0,
+            explanation="Enter the combined PT cut value in GeV.",
         )
 
         self.ptMaxCutInput = self.addFieldWithExplanation(
-            "PT Max Cut (GeV):", QDoubleSpinBox(self), max_value=100, decimals=2, default_value=50,
-            explanation="Enter the maximum PT cut value in GeV."
+            "PT Max Cut (GeV):",
+            QDoubleSpinBox(self),
+            max_value=100,
+            decimals=2,
+            default_value=50,
+            explanation="Enter the maximum PT cut value in GeV.",
         )
 
         self.nclusPtCutInput = self.addFieldWithExplanation(
-            "NCLUS PT Cut (GeV):", QDoubleSpinBox(self), max_value=100, decimals=2, default_value=0.0,
-            explanation="Enter the NCLUS PT cut value in GeV."
+            "NCLUS PT Cut (GeV):",
+            QDoubleSpinBox(self),
+            max_value=100,
+            decimals=2,
+            default_value=0.0,
+            explanation="Enter the NCLUS PT cut value in GeV.",
         )
 
         self.positSmearingFactorInput = self.addFieldWithExplanation(
-            "Position Smearing Factor:", QDoubleSpinBox(self), max_value=10, decimals=2, default_value=2.8,
-            explanation="Enter the position smearing factor."
+            "Position Smearing Factor (mm):",
+            QDoubleSpinBox(self),
+            max_value=10,
+            decimals=2,
+            default_value=2.8,
+            explanation="Enter the position smearing factor.",
         )
 
         self.saveToTreeInput = self.addFieldWithExplanation(
-            "Save to Tree:", QCheckBox(self), default_value=False,
-            explanation="Check to save the results to a tree."
+            "Save to Tree:",
+            QCheckBox(self),
+            default_value=False,
+            explanation="Check to save the results to a tree.",
         )
 
         self.baseSmearPercentInput = self.addFieldWithExplanation(
-            "Base Smearing Percent:", QDoubleSpinBox(self), max_value=1, decimals=3, default_value=0.168,
-            explanation="Enter the base smearing percent."
+            "Base Smearing Percent:",
+            QDoubleSpinBox(self),
+            max_value=1,
+            decimals=3,
+            default_value=0.168,
+            explanation="Enter the base smearing percent.",
         )
 
         self.nStepsInput = self.addFieldWithExplanation(
-            "Number of Steps:", QSpinBox(self), max_value=100, default_value=25,
-            explanation="Enter the number of steps for the simulation."
+            "Number of Steps:",
+            QSpinBox(self),
+            max_value=100,
+            default_value=1,
+            explanation="Enter the number of steps for the simulation.",
         )
 
         self.stepSizeInput = self.addFieldWithExplanation(
-            "Step Size:", QDoubleSpinBox(self), max_value=1, decimals=4, step=0.0001, default_value=0.001,
-            explanation="Enter the step size for the simulation."
+            "Step Size:",
+            QDoubleSpinBox(self),
+            max_value=1,
+            decimals=4,
+            step=0.0001,
+            default_value=0.001,
+            explanation="Enter the step size for the simulation.",
         )
 
         self.outputFormatInput = self.addFieldWithExplanation(
-            "Output Format:", QComboBox(self), explanation="Select the format for saving the output."
+            "Output Format:",
+            QComboBox(self),
+            explanation="Select the format for saving the output.",
         )
         self.outputFormatInput.addItems(["PDF", "PNG", "JPEG"])
 
@@ -155,7 +228,8 @@ class SimulationGUI(QWidget):
         self.runButton.clicked.connect(self.runSimulation)
         self.layout.addWidget(self.runButton)
 
-        self.setStyleSheet("""
+        self.setStyleSheet(
+            """
             QLabel {
                 font-size: 18px;
             }
@@ -168,15 +242,27 @@ class SimulationGUI(QWidget):
             QScrollArea {
                 background-color: white;
             }
-        """)
+        """
+        )
 
         self.loadParams()
 
-    def addFieldWithExplanation(self, label_text, widget, max_value=None, decimals=None, step=None, default_value=None, explanation=None):
+    def addFieldWithExplanation(
+        self,
+        label_text,
+        widget,
+        max_value=None,
+        decimals=None,
+        step=None,
+        default_value=None,
+        explanation=None,
+    ):
         label = QLabel(label_text, self)
         explanation_label = QLabel(self)
         explanation_pixmap = QPixmap(self.icon_file)
-        explanation_pixmap = explanation_pixmap.scaled(20, 20, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        explanation_pixmap = explanation_pixmap.scaled(
+            20, 20, Qt.KeepAspectRatio, Qt.SmoothTransformation
+        )
         explanation_label.setPixmap(explanation_pixmap)
         explanation_label.setToolTip(explanation)
         explanation_label.setCursor(Qt.PointingHandCursor)
@@ -218,37 +304,47 @@ class SimulationGUI(QWidget):
         return widget
 
     def loadParams(self):
-        if not os.path.exists('guiconfig'):
-            os.makedirs('guiconfig')
-            
+        if not os.path.exists("guiconfig"):
+            os.makedirs("guiconfig")
+
         if os.path.exists(self.params_file):
             with open(self.params_file, "r") as file:
                 params = json.load(file)
-                self.particleTypeInput.setCurrentText(params.get("particleType", "Pion"))
+                self.particleTypeInput.setCurrentText(
+                    params.get("particleType", "Pion")
+                )
                 self.nParticlesInput.setValue(params.get("nParticles", 8000000))
                 self.ptMaxInput.setValue(params.get("ptMax", 50))
                 self.ptMinInput.setValue(params.get("ptMin", 0))
-                self.weightMethodInput.setCurrentText(params.get("weightMethod", "WSHP"))
+                self.weightMethodInput.setCurrentText(
+                    params.get("weightMethod", "WSHP")
+                )
                 self.asymmCutInput.setChecked(params.get("applyAsymmCut", True))
                 self.asymmCutValueInput.setValue(params.get("asymmCutValue", 0.6))
                 self.clusterOverlapInput.setChecked(params.get("clusterOverlap", True))
-                self.clusterOverlapProbInput.setValue(params.get("clusterOverlapProb", 0.99))
+                self.clusterOverlapProbInput.setValue(
+                    params.get("clusterOverlapProb", 0.99)
+                )
                 self.deltaRcutMaxInput.setValue(params.get("deltaRcutMax", 1.1))
                 self.pt1cutInput.setValue(params.get("pt1cut", 1.5))
                 self.pt2cutInput.setValue(params.get("pt2cut", 1.5))
                 self.combPtCutInput.setValue(params.get("combPtCut", 0))
                 self.ptMaxCutInput.setValue(params.get("ptMaxCut", 50))
                 self.nclusPtCutInput.setValue(params.get("nclusPtCut", 0.0))
-                self.positSmearingFactorInput.setValue(params.get("positSmearingFactor", 2.8))
+                self.positSmearingFactorInput.setValue(
+                    params.get("positSmearingFactor", 2.8)
+                )
                 self.saveToTreeInput.setChecked(params.get("saveToTree", False))
-                self.baseSmearPercentInput.setValue(params.get("baseSmearPercent", 0.168))
+                self.baseSmearPercentInput.setValue(
+                    params.get("baseSmearPercent", 0.168)
+                )
                 self.nStepsInput.setValue(params.get("nSteps", 25))
                 self.stepSizeInput.setValue(params.get("stepSize", 0.001))
                 self.outputFormatInput.setCurrentText(params.get("outputFormat", "PDF"))
 
     def saveParams(self):
-        if not os.path.exists('guiconfig'):
-            os.makedirs('guiconfig')
+        if not os.path.exists("guiconfig"):
+            os.makedirs("guiconfig")
 
         params = {
             "particleType": self.particleTypeInput.currentText(),
@@ -317,9 +413,7 @@ class SimulationGUI(QWidget):
         outputFormat = self.outputFormatInput.currentText().lower()
 
         script = (
-            "pion_simulation_script"
-            if particleType == "pion"
-            else "eta_simulation_script"
+            "gen_res"
         )
 
         command = (
@@ -332,25 +426,25 @@ class SimulationGUI(QWidget):
         print("Running command:", command)
 
         # Uncomment the following line to actually run the command
-        # os.system(command)
+        os.system(command)
 
         params = {
             "Particle Type": self.particleTypeInput.currentText(),
             "Number of Particles": self.nParticlesInput.value(),
-            "PT Max": self.ptMaxInput.value(),
-            "PT Min": self.ptMinInput.value(),
+            "PT Max (GeV)": self.ptMaxInput.value(),
+            "PT Min (GeV)": self.ptMinInput.value(),
             "Weight Method": self.weightMethodInput.currentText(),
             "Apply Asymm Cut": self.asymmCutInput.isChecked(),
             "Asymm Cut Value": self.asymmCutValueInput.value(),
             "Cluster Overlap": self.clusterOverlapInput.isChecked(),
             "Cluster Overlap Probability": self.clusterOverlapProbInput.value(),
             "Delta R Cut Max": self.deltaRcutMaxInput.value(),
-            "PT1 Cut": self.pt1cutInput.value(),
-            "PT2 Cut": self.pt2cutInput.value(),
-            "Combined PT Cut": self.combPtCutInput.value(),
-            "PT Max Cut": self.ptMaxCutInput.value(),
-            "NCLUS PT Cut": self.nclusPtCutInput.value(),
-            "Position Smearing Factor": self.positSmearingFactorInput.value(),
+            "PT1 Cut (GeV)": self.pt1cutInput.value(),
+            "PT2 Cut (GeV)": self.pt2cutInput.value(),
+            "Combined PT Cut factor": self.combPtCutInput.value(),
+            "PT Max Cut (GeV)": self.ptMaxCutInput.value(),
+            "NCLUS PT Cut (GeV)": self.nclusPtCutInput.value(),
+            "Position Smearing Factor (mm)": self.positSmearingFactorInput.value(),
             "Save to Tree": self.saveToTreeInput.isChecked(),
             "Base Smearing Percent": self.baseSmearPercentInput.value(),
             "Number of Steps": self.nStepsInput.value(),
@@ -360,8 +454,8 @@ class SimulationGUI(QWidget):
             "Timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         }
 
-        if not os.path.exists('pioncode/canvas_pdf'):
-            os.makedirs('pioncode/canvas_pdf')
+        if not os.path.exists("pioncode/canvas_pdf"):
+            os.makedirs("pioncode/canvas_pdf")
 
         filename = f"pioncode/canvas_pdf/simulation_params_{run_number}.{outputFormat}"
         if outputFormat == "pdf":
@@ -370,8 +464,8 @@ class SimulationGUI(QWidget):
             self.printParametersToImage(params, filename)
 
     def printParametersToPDF(self, params, filePath):
-        if not os.path.exists('pioncode/canvas_pdf'):
-            os.makedirs('pioncode/canvas_pdf')
+        if not os.path.exists("pioncode/canvas_pdf"):
+            os.makedirs("pioncode/canvas_pdf")
 
         c = canvas.Canvas(filePath, pagesize=letter)
         width, height = letter
@@ -384,8 +478,8 @@ class SimulationGUI(QWidget):
         print(f"Parameters saved to {filePath}")
 
     def printParametersToImage(self, params, filePath):
-        if not os.path.exists('pioncode/canvas_pdf'):
-            os.makedirs('pioncode/canvas_pdf')
+        if not os.path.exists("pioncode/canvas_pdf"):
+            os.makedirs("pioncode/canvas_pdf")
 
         fig, ax = plt.subplots(figsize=(8.5, 11))
         ax.axis("off")
@@ -403,6 +497,7 @@ class SimulationGUI(QWidget):
         plt.savefig(filePath, bbox_inches="tight")
         plt.close()
         print(f"Parameters saved to {filePath}")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
